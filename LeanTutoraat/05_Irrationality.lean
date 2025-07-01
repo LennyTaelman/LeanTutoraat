cb57e0b98726873cf10608524309b34cf02ae113
LennyTaelman <lenny.taelman@gmail.com>
2025-07-01 21:09:59 +0200

    progress

--- LeanTutoraat/05_Irrationality.lean
+++ LeanTutoraat/05_Irrationality.lean
@@ -14,8 +14,6 @@ noncomputable section
   lemma factorial_succ (n : ℕ) : (n + 1) ! = (n + 1) * n ! := by
 -/
 
-#check Nat.factorial_succ
-
 def fac (n : ℕ) : ℝ :=
   match n with
   | 0 => 1
@@ -24,6 +22,8 @@ def fac (n : ℕ) : ℝ :=
 lemma fac_zero : fac 0 = 1 := by rfl
 
 lemma fac_succ (n : ℕ) : fac (n + 1) = (n + 1) * fac n := by rfl
+
+lemma fac_one : fac 1 = 1 := by rw [fac_succ]; rw [fac_zero]; numbers
 
 lemma fac_ge_one (n : ℕ) : fac n ≥ 1 := by
   simple_induction n with n IH
@@ -39,6 +39,7 @@ lemma fac_gt_zero (n : ℕ) : fac n > 0 := by
   calc fac n ≥ 1 := by apply fac_ge_one
     _ > 0 := by numbers
 
+example (x y : ℝ) (h : x ≥ 0) (h2 : y ≥ 0) : x * y ≥ 0 := by exact mul_nonneg h h2
 
 lemma fac_bound (n : ℕ) (k : ℕ) (hn : n > 0) :
     fac (n + k) ≥ 2 ^ k * fac n := by
@@ -48,7 +49,9 @@ lemma fac_bound (n : ℕ) (k : ℕ) (hn : n > 0) :
       _ ≥ 1 * fac n := by addarith []
   · -- inductive step
     have h : (n : ℝ) + (k : ℝ) + 1 ≥ 2 := by norm_cast; addarith [hn]
-    have h2 : 2 ^ k * fac n > 0 := by sorry
+    have h2 : (2 : ℝ) ^ k > 0 := by positivity
+    have h3 : 2 ^ k * fac n > 0 := by
+      exact mul_pos h2 (fac_gt_zero n)
     calc fac (n + (k + 1)) = fac (n + k + 1) := by ring
       _ = (n + k + 1) * fac (n + k) := by rw [fac_succ]; norm_cast
       _ ≥ (n + k + 1) * (2 ^ k * fac n) := by rel [IH]
@@ -77,7 +80,6 @@ lemma fac_div_fac_integral (n : ℕ) (m : ℕ) (h : n ≥ m) :
   Propositie: ∑_{i=0}^{k-1} 1 / (2 ^ i : ℝ) ≤ 2
 -/
 
-#check Finset.sum
 
 lemma geometric_sum (k : ℕ) : ∑ i in range k, 1 / (2 : ℝ) ^ i = 2 - 2 / (2 : ℝ) ^ k := by
   simple_induction k with k IH
@@ -90,7 +92,8 @@ lemma geometric_sum (k : ℕ) : ∑ i in range k, 1 / (2 : ℝ) ^ i = 2 - 2 / (2
 
 lemma geometric_sum_lt_2 (k : ℕ) : ∑ i in range k, 1 / (2 : ℝ) ^ i < 2 := by
   rw [geometric_sum]
-  sorry
+  apply sub_lt_self
+  positivity
 
 /-
   De rij a_n die naar e convergeert. De termen zijn:
@@ -100,42 +103,56 @@ lemma geometric_sum_lt_2 (k : ℕ) : ∑ i in range k, 1 / (2 : ℝ) ^ i < 2 := 
   Note that a n consists of n terms
 -/
 
-def a (n : ℕ) := ∑ i in range n, 1 / fac i
+-- type using \inv
+def b (n : ℕ) := (fac n)⁻¹
+
+lemma b0 : b 0 = 1 := by unfold b; rw [fac_zero]; numbers
+
+lemma b1 : b 1 = 1 := by unfold b; rw [fac_one]; numbers
+
+lemma b_pos (n : ℕ) : b n > 0 := by
+  apply inv_pos.mpr -- TODO: find better lemma name for this
+  exact fac_gt_zero n
+
+def a (n : ℕ) := ∑ i in range n, b i
 
 lemma a0 : a 0 = 0 := by rfl
 
-lemma a1 : a 1 = 1 := by unfold a; norm_num; exact fac_zero
-
+lemma a1 : a 1 = 1 := by unfold a; rw [sum_range_one]; rw [b0]
 
 lemma le_add_pos  (x y : ℝ) (h : 0 ≤ y) : x ≤ x + y := by
   addarith [h]
 
-lemma a_succ (n : ℕ) : a (n + 1) = a n + 1 / (n ! : ℝ) := by
+lemma a_succ (n : ℕ) : a (n + 1) = a n + 1 / fac n := by
   unfold a
   rw [sum_range_succ]
+  congr
 
 
-lemma a_monotone (n : ℕ) : a n ≤ a (n + 1) := by
+  sorry
+
+
+lemma a_monotone (n : ℕ) : a n < a (n + 1) := by
   unfold a
   rw [sum_range_succ]
-  have h : 0 ≤ 1 / (n ! : ℝ) := by positivity
-  apply le_add_pos
-  exact h
+  rw [lt_add_iff_pos_right]
+  exact fac_inv_pos n
 
 
 lemma factorial_a_succ (n : ℕ) :
-    (n + 1)! * a (n + 2) = (n + 1) * n ! * a (n + 1) + 1 := by
+    (fac (n + 1)) * a (n + 2) = (n + 1) * (fac n) * a (n + 1) + 1 := by
   rw [a_succ]
-  rw [factorial_succ]
-  field_simp
-  ring
+  rw [fac_succ]
+  sorry
 
-lemma a_integrality (n : ℕ) : ∃ m : ℕ, n ! * a (n + 1) = m := by
+lemma a_integrality (n : ℕ) : ∃ m : ℕ, (fac n) * a (n + 1) = m := by
   simple_induction n with n IH
   · -- base case
     use 1
     rw [a]
     norm_num
+    rw [fac_zero]
+    numbers
   · -- inductive step
     obtain ⟨m, hm⟩ := IH
     use (n + 1) * m + 1
@@ -168,8 +185,6 @@ lemma a_below_e (n : ℕ) : a n < e := by sorry
 
 theorem a_to_e : ∀ ε > 0, ∃ N : ℕ, e < a N + ε := by sorry
 
-
-#min_imports
 
 
 /-

70da2b228faecb55e54ff697b427af24e88f7fea
LennyTaelman <lenny.taelman@gmail.com>
2025-08-06 16:48:55 +0200

    file with critical things that come up in Irrationality

--- LeanTutoraat/Bottlenecks.lean
+++ LeanTutoraat/Bottlenecks.lean
@@ -11,27 +11,41 @@ noncomputable section
 
 
 /-
-  Various field inequalities
+  Collection of `minimal' statements that we should be able to handle idiomatically.
 -/
 
-example (x : ℝ) (h : x > 0) : x⁻¹ > 0 := by exact inv_pos.mpr h
-example (x : ℝ) (h : x > 0) : x⁻¹ > 0 := by positivity
-example (x : ℝ) (h : x > 0) : x⁻¹ > 0 := by apply inv_pos.mpr; exact h
-example (x : ℝ) (h : x > 0) : x⁻¹ > 0 := by field_simp; exact one_div_pos.mpr h;
-example (x : ℝ) (h : x > 0) : x⁻¹ > 0 := by simp_all only [gt_iff_lt, inv_pos];
-
-example (x : ℝ) (h : x > 0) (h2 : y ≥ 0) : x * y ≥ 0 := by simp_all only [gt_iff_lt,
-  ge_iff_le, zero_le_mul_left]
-
-example (n : ℕ) : 2 / ((n:ℝ) + 1) > 0 := by positivity
-
-example (n : ℕ) : 3 / ((5 : ℝ) ^ n) > 0 := by positivity
-
-
-/-
-  Things specific to natural and integers
--/
 
 example (n : ℕ) (h : n > 0) : n ≥ 1 := by apply h
 
-example (n : ℤ) (h : n > 0) : n ≥ 1 := by apply h
+example (c : ℝ) (h : c > 1) : 1 / c < 1 := by
+  have h2 : 1 / c > 0 := by positivity
+  calc
+    1 / c =  1 * (1 / c) := by ring
+    _ < c * (1 / c) := by rel [h]
+    _ = 1 := by field_simp
+
+example (c : ℝ) (h : c ≠ 0) : c * (1 / c) = 1 := by field_simp
+
+
+/-
+  The examples below feel satisfactory
+-/
+example (a : ℝ) : a ≥ a := by rfl
+
+example (a : ℝ) : a ≤ a := by rfl
+
+example (a : ℝ) : a = a := by rfl
+
+example (f : ℕ → ℝ) (n m : ℕ): f (n + m) = f (m + n) := by ring
+
+example (a : ℝ) (k : ℕ) : a ^ k * a ^ k = a ^ (2 * k) := by ring
+
+example (a : ℝ) (k : ℕ) : a ^ k * a = a ^ (1 + k) := by ring
+
+example (a b : ℝ) (h : b > 0) : a + b > a := by extra
+
+example (a b : ℝ) (h : b > 0) : a - b < a := by extra
+
+example (n m : ℕ) (a : ℝ) : (n * m : ℕ) * a = (n : ℕ) * (m * a) := by
+  push_cast
+  ring

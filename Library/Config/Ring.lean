97c66b45d6c54ea881ffb03d774d79209fd2e374
LennyTaelman <lenny.taelman@gmail.com>
2025-09-09 10:48:26 +0200

    algebra error message

--- Library/Config/Ring.lean
+++ Library/Config/Ring.lean
@@ -9,6 +9,13 @@ nonterminal use. -/
 macro_rules | `(conv | ring) => `(conv | ring_nf)
 macro "ring" : tactic => `(tactic | first | (ring_nf; done) | ring1)
 
-macro "algebra" : tactic => `(tactic | (push_cast; first | ring | (field_simp; ring) | (field_simp; done)))
+/--
+The `algebra` tactic attempts to prove algebraic identities involving addition, multiplication,
+subtraction, and division.
+-/
+macro "algebra" : tactic =>
+  `(tactic | first |
+    (push_cast; first | ring | (field_simp; ring) | (field_simp; done)) |
+    fail "algebra failed. Perhaps the goal is not an algebraic identity, or the identity is false?")
 
 -- macro "algebra" : tactic => `(tactic | first | ring | field_simp | (field_simp; ring))

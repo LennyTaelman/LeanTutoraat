6ce9c8b13e854bcdef33f366f70cb3cb3dc6975b
LennyTaelman <lenny.taelman@gmail.com>
2025-10-01 15:12:42 +0200

    tweak proofs

--- LeanTutoraat/02_Structure.lean
+++ LeanTutoraat/02_Structure.lean
@@ -116,76 +116,65 @@ example (x y a b : ℝ) (h1 : x ≤ y) (h2 : a ≤ b) : a * y + b * x ≤ a * x 
 /-
   This looks inefficient! We are doing the same proof twice. We should extract the
   repeated part into a lemma. Replace the `sorry` below with a correct proof.
+
+  Note that the statement of a `lemma` is the same as that of an `example`, but
+  that it requires a name (so we can refer to it). Do you understand the choice
+  of name?
 -/
 
 lemma sub_nonneg_of_ge (x y : ℝ) (h : x ≤ y) : 0 ≤ y - x := by
   sorry
 
+-- now the proof is much shorter
 example (x y a b : ℝ) (h1 : x ≤ y) (h2 : a ≤ b) : a * y + b * x ≤ a * x + b * y := by
-  have h1' : 0 ≤ y - x := by apply sub_nonneg_of_ge x y h1
-  have h2' : 0 ≤ b - a := by apply sub_nonneg_of_ge a b h2
+  have h1' : 0 ≤ y - x := by
+    apply sub_nonneg_of_ge x y h1
+  have h2' : 0 ≤ b - a := by
+    apply sub_nonneg_of_ge a b h2
   calc
     a * y + b * x ≤ (a * y + b * x) + (b - a) * (y - x) := by extra
                 _ = a * x + b * y                       := by algebra
 
+/-
+  Note that the lemma takes three arguments: two real numbers and the fact that
+  the first is less than the second.
+
+  We can now re-use the lemma in other proofs:
+-/
 
 
-
--- and we can re-use the lemma in other proofs:
-
--- TODO: example!
-
-
-
-
-
-
-
-
-
-
-example {t : ℝ} (h1 : t ^ 2 = 3 * t) (h2 : t ≥ 1) : t ≥ 2 := by
-  have h3 :=
-  calc t * t = t ^ 2 := by ring
-    _ = 3 * t := by rw [h1]
-  cancel t at h3
-  addarith [h3]
-
-
-example {a b : ℝ} (h1 : a ^ 2 = b ^ 2 + 1) (h2 : a ≥ 0) : a ≥ 1 := by
-  have h3 :=
-  calc
-    a ^ 2 = b ^ 2 + 1 := by rw [h1]
-    _ ≥ 1 := by extra
-    _ = 1 ^ 2 := by ring
-  cancel 2 at h3
-
-
-example {x y : ℤ} (hx : x + 3 ≤ 2) (hy : y + 2 * x ≥ 3) : y > 3 := by
-  sorry
 
 example (a b : ℝ) (h1 : -b ≤ a) (h2 : a ≤ b) : a ^ 2 ≤ b ^ 2 := by
+  have h1' : 0 ≤ a - (-b) := by
+    sorry
+  have h2' : 0 ≤ b - a := by
+    sorry
+  calc
+    a ^ 2 ≤ a ^ 2 + (b - a) * (a - (-b)) := by sorry
+        _ = b ^ 2                        := by sorry
+
+
+-- hint: "complete the square"
+example (a b : ℝ) : a ^ 2 + a * b + b ^ 2 ≥ 0 := by
   sorry
+
+/-
+  Now turn the example above into a lemma, and use it to prove the statement
+  below. (A more natural proof would be to distinguish cases depending on the
+  sign of `a` and `b`. We will see how to do that later.)
+-/
 
 example (a b : ℝ) (h : a ≤ b) : a ^ 3 ≤ b ^ 3 := by
   sorry
 
-/-! # Exercises -/
 
-
-
-
-example {n : ℤ} (hn : n ^ 2 + 4 = 4 * n) : n = 2 := by
-  sorry
-
-example (x y : ℚ) (h : x * y = 1) (h2 : x ≥ 1) : y ≤ 1 := by
-  sorry
-
-
-/-! ## Invoking a lemma with `apply` -/
 
 
 /-! ## Proving ∀-statements with `intro` -/
 
 
 /-! ## Proving ∃-statements with `use` -/
+
+
+
+/-! # Extra challenges -/

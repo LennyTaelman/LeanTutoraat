645f3cc88d4f17ba5b7c730787ac860b6b61cce8
LennyTaelman <lenny.taelman@gmail.com>
2025-10-29 11:00:51 +0100

    first inductive proofs

--- LeanTutoraat/03_Induction.lean
+++ LeanTutoraat/03_Induction.lean
@@ -1,10 +1,9 @@
-import Mathlib.Data.Real.Basic
 import Library.Basic
 
-tutoraat_init
+/-! # Induction -/
 
-namespace Nat
 
+/-! ## Proving something holds for all natural numbres with `simple_induction` -/
 
 
 /-
@@ -18,132 +17,154 @@ namespace Nat
 -/
 
 
--- Here is a simple example. Replace all the `sorry`s with proofs.
+/-
+  Below is a simple example. Note the two dots `·` after `simple_induction`.
+  These indicate the two goals: base case and inductive step. Put your cursor
+  after these dots to see the goals.
+
+  Note that after the second dot, the induction hypothesis `IH` appears as a
+  new hypothesis that is now available in the proof.
+
+  Replace all the `sorry`s with proofs. Each one can be handled by a single tactic.
+-/
 
 example (n : ℕ) : 2 ^ n ≥ n + 1 := by
   simple_induction n with k IH
-  · -- base case: goal is 2 ^ 0 ≥ 0 + 1
-    sorry
-  · -- inductive step: goal is 2 ^ (k + 1) ≥ (k + 1) + 1
-    -- note the new hypothesis `IH`!
-    calc
+  · sorry
+  · calc
       2 ^ (k + 1) = 2 * 2 ^ k           := by sorry
-                _ ≥ 2 * (k + 1)         := by sorry
-                _ = ((k + 1) + 1) + k   := by sorry
                 _ ≥ (k + 1) + 1         := by sorry
 
 
--- Now prove Bernoulli's inequality:
-
-example (x : ℝ) (n : ℕ) (h : x ≥ 0) : (1 + x) ^ n ≥ 1 + n * x := by
-  simple_induction n with k IH
-  · -- base case: goal is `(1 + x) ^ 0 ≥ 1 + 0 * x`
-    sorry
-  · -- inductive step: goal is `(1 + x) ^ (k + 1) ≥ 1 + (k + 1) * x`
-    calc (1 + x) ^ (k + 1) = (1 + x) * (1 + x) ^ k := by algebra
-      _ ≥ (1 + x) * (1 + k * x) := by rel [IH]
-      _ = 1 + (k + 1) * x +  k * x ^ 2 := by algebra
-      _ ≥ 1 + (k + 1) * x := by extra
-
-
--- Note: in the right window pane you may have seen expressions like `↑k`. These
--- indicate that the natural number `k` is being considered as a real number.
-
-
 /-
-  Now give your own inductive proof for the following statement.
-  To typeset the dots for base and inductive case, use `\.`
+  Now fill in the proofs of base and inductive step in the following proof.
 -/
 
 example (n : ℕ) : n ^ 2 ≥ n := by
+  simple_induction n with k IH
+  · sorry
+  · sorry
+
+/-
+  Finally, write an induction proof for the following statement.
+
+  To typeset the dots for base and inductive case, use `\.` Note that the proofs
+  for base and inductive step are indented.
+-/
+
+example (n : ℕ) : 3 ^ n ≥ n ^ 2 + n + 1 := by
   sorry
+
+
+/-! ## Prove something holds for all `n ≥ C` with `induction_from_starting_point` -/
 
 
 /-
   Sometimes we want to prove something for all n ≥ C,
   and hence want to start induction at n = C.
 
-  If `n` is a natural number and `hn` the hypothesis that `n ≥ C`,
-  then the tactic `induction_from_starting_point n, hn with k hk IH`
+  If `n` is a natural number and `h` the hypothesis that `C ≤ n`,
+  then the tactic `induction_from_starting_point n, h with k hk IH`
   creates two goals:
   - a base case: prove the statement for `n = C`
-  - an inductive step: prove the statement for `n = k + 1`,
-    where the induction hypothesis `IH` is that the statement holds for `n = k`
+  - an inductive step: prove the statement for `n = k + 1`, where
+    * `hk` is the assumption that `k ≥ C`
+    * `IH` is the induction hypothesis (the statement holds for `n = k`)
+
+  Place your cursor after the two dots to see the goals. Then replace the
+  sorry's with proofs.
 -/
 
-example (n : ℕ) (hn : 2 ≤ n) : 3 ^ n ≥ 2 ^ n + 5 := by
+example (n : ℕ) (hn : n ≥ 2) : n ^ 2 ≥ n + 2 := by
   induction_from_starting_point n, hn with k hk IH
-  · -- base case
-    numbers
-  · -- inductive step
-    calc 3 ^ (k + 1) = 2 * 3 ^ k + 3 ^ k := by algebra
-      _ ≥ 2 * (2 ^ k + 5) + 3 ^ k := by rel [IH]
-      _ = 2 ^ (k + 1) + 5 + (5 + 3 ^ k) := by algebra
-      _ ≥ 2 ^ (k + 1) + 5 := by extra
+  · sorry
+  · calc
+      (k + 1) ^ 2  = k ^ 2 + 2 * k + 1 := by sorry
+                 _ ≥ (k + 1) + 2       := by sorry
 
+/-
+  Now do the following one yourself
+-/
 
-example (n : ℕ) (hn : n ≥ 2) : 2 ^ n ≥ n ^ 2 := by
+example (n : ℕ) (hn : n ≥ 3) : n ^ 2 ≥ 3 * n := by
   induction_from_starting_point n, hn with k hk IH
-  · -- base case
-    sorry
-  · -- inductive step
-    sorry
+  · sorry
+  · calc
+      (k + 1) ^ 2 = k ^ 2 + 2 * k + 1 := by algebra
+                 _ ≥  3 * (k + 1) := by linarith
 
 
-/- # Exercises -/
+/-
+  One more variation. In this one, we show that `n ^ 2 ≥ n + 100` holds for if
+  `n` is sufficiently large. We state it as `∃ C : ℕ, n ≥ C → n ^ 2 ≥ n + 100`,
+  where the arrow `→` should be read as an implication.
 
+  Prove this in lean, by following these steps:
+  1. Figure out what value of `C` you will use. (Say `C = 5` -- which won't work!)
+  2. Start the proof with `use 5`. The goal is now `n ≥ 5 → n ^ 2 ≥ n + 100`
+  3. Use the tactic `intro h` to introduce the hypothesis `n ≥ 5`.
+  4. Finish the proof using induction.
+-/
 
-example (n : ℕ) : 3 ^ n ≥ n ^ 2 + n + 1 := by
+example (n : ℕ) : ∃ C : ℕ, n ≥ C → n ^ 2 ≥ n + 100 := by
   sorry
 
--- this is a stronger form of the Bernoulli inequality from above.
-example (x : ℝ) (h : -1 ≤ x) (n : ℕ) : (1 + x) ^ n ≥ 1 + n * x := by
-  sorry
 
-example : ∃ C : ℕ, ∀ n : ℕ, n ≥ C → 3 ^ n ≥ 2 ^ n + 100 := by
-  sorry
 
 
 /- # Finite sums -/
 
 
 /-
-  We now show that for every `n : ℕ` we have
-  `1 + 2 + 4 + ... + 2 ^ (n - 1) = 2 ^ n - 1`
+  Induction is a powerful tool for proving statements about finite sums of the
+  form `Σ_{i=0}^{n} a_i`.
+
+  As a first example, let us prove that for every `n : ℕ` we have
+    `1 + 2 + 4 + ... + 2 ^ (n - 1) = 2 ^ n - 1`
+
   First we define the left-hand side as a function
-  `g : ℕ → ℝ` which maps `n` to `1 + 2 + 4 + ... + 2 ^ (n - 1)`.
+  `s : ℕ → ℕ` which maps `n` to `1 + 2 + 4 + ... + 2 ^ (n - 1)`.
 -/
 
-def c (n : ℕ) : ℝ :=
+def s (n : ℕ) : ℕ :=
   match n with
   | 0 => 0
-  | n + 1 => c n + 2 ^ n
+  | n + 1 => s n + 2 ^ n
 
 /-
-  You can ignore the precise form of the definition. All you need to know is that `c n` is a real
-  number, and that it satisfies the lemmas `c_zero` and `c_succ` below.
+  The above defines `s 0` to be `0` (the empty sum) and `s (n + 1)` to be `s n +
+  2 ^ n`. Verify for yourself (on paper if necessary) that this indeed forces
+    `s n = 1 + 2 + 4 + ... + 2 ^ (n - 1)`.
 
-  The only thing you should every use about `c` in your proofs are those two lemmas.
+  You can ignore the precise form of the definition. All you need to know about
+  `s` is
+  1. `s n` is a natural number
+  2. `s 0 = 0`, which is proven in the lemma `s_zero` below.
+  3. `s (n + 1) = s n + 2 ^ n`, which is proven in the lemma `s_succ` below.
+
+  In your proofs, you can *use*`rewrite [s_zero]` or `rewrite [s_succ]` to
+  transform `s 0` into `0` or `s (n + 1)` into `s n + 2 ^ n`.
 -/
 
-lemma c_zero : c 0 = 0 := by rfl
+lemma s_zero : s 0 = 0 := by rfl
 
-lemma c_succ (n : ℕ) : c (n + 1) = c n + 2 ^ n := by rfl
+lemma s_succ (n : ℕ) : s (n + 1) = s n + 2 ^ n := by rfl
 
 
+/-
+  We can now prove that `s n = 2 ^ n - 1` for all `n : ℕ`. We state it slightly
+  differently, as `1 + s n = 2 ^ n`, to avoid having to deal with subtraction in `ℕ`.
+-/
 
-example (n : ℕ) : c n = 2 ^ n - 1 := by
+
+example (n : ℕ) : 1 + s n = 2 ^ n := by
   simple_induction n with k IH
-  · -- base case: ⊢ c 0 = ...
-    rw [c_zero]
-    numbers
-  · -- inductive step: ⊢ c (k + 1) = ...
-    rw [c_succ]
-    rw [IH]
-    algebra
-
-
-
+  · sorry
+  · calc
+      1 + s (k + 1) = 1 + (s k + 2 ^ k)  := by sorry
+                  _ = (1 + s k) + 2 ^ k  := by sorry
+                  _ = 2 ^ k + 2 ^ k      := by sorry
+                  _ = 2 ^ (k + 1)        := by sorry
 
 
 
@@ -175,17 +196,11 @@ lemma s1_succ (n : ℕ) : s1 (n + 1) = s1 n + (n + 1) := by rfl
 
 example (n : ℕ) : 2 * s1 n = n * (n + 1) := by
   simple_induction n with k IH
-  · -- base case: ⊢ s1 0 = ...
-    rw [s1_zero]
-  · -- inductive step: ⊢ s1 (k + 1) = ...
-    rw [s1_succ]
-    calc
-      2 * (s1 k + (k + 1)) = 2 * s1 k + 2 * (k + 1) := by algebra
-      _ = k * (k + 1) + 2 * (k + 1) := by rw [IH]
-      _ = (k + 1) * (k + 1 + 1) := by algebra
+  · sorry
+  · sorry
 
 /-
-  Consider s2 n = 1 + 2^2 + 3^2 + ... + n^2.
+  Let's move to squares! Consider s2 n = 1 + 2 ^ 2 + 3 ^ 2 + ... + n ^ 2.
 -/
 
 def s2 (n : ℕ) : ℕ :=
@@ -198,47 +213,21 @@ lemma s2_zero : s2 0 = 0 := by rfl
 lemma s2_succ (n : ℕ) : s2 (n + 1) = s2 n + (n + 1) ^ 2 := by rfl
 
 
+/-
+  Write a proof that `6 * s2 n = n * (n + 1) * (2 * n + 1)`.
+-/
 example (n : ℕ) : 6 * s2 n = n * (n + 1) * (2 * n + 1) := by
-  simple_induction n with k IH
-  · -- base case: s2 0 = ...
-    rw [s2_zero]
-  · -- inductive step: s2 (k + 1) = ...
-    rw [s2_succ]
-    calc
-      6 * (s2 k + (k + 1) ^ 2) = 6 * s2 k + 6 * (k + 1) ^ 2 := by algebra
-      _ = k * (k + 1) * (2 * k + 1) + 6 * (k + 1) ^ 2 := by rw [IH]
-      _ = (k + 1) * (k + 1 + 1) * (2 * (k + 1) + 1) := by algebra
+  sorry
 
-
-
-def s3 (n : ℕ) : ℕ :=
-  match n with
-  | 0 => 0
-  | n + 1 => (s3 n) + (n + 1) ^ 3
-
-lemma s3_zero : s3 0 = 0 := by rfl
-
-lemma s3_succ (n : ℕ) : s3 (n + 1) = s3 n + (n + 1) ^ 3 := by rfl
 
 
 /-
-  Let us prove that s2 n ≥ n^3 / 3.
+  Variation: prove that `3 * s2 n ≥ n ^ 3`.
 -/
 
 
 example (n : ℕ) : 3 * s2 n ≥ n ^ 3 := by
-  simple_induction n with k IH
-  · -- base case: s2 0 ≥ 0
-    rw [s2_zero]
-    numbers
-  · -- inductive step: s2 (k + 1) ≥ (k + 1) ^ 3 / 3
-    rw [s2_succ]
-    calc
-      3 * (s2 k + (k + 1) ^ 2) = 3 * s2 k + 3 * (k + 1) ^ 2 := by algebra
-      _ ≥ k ^ 3 + 3 * (k + 1) ^ 2 := by rel [IH]
-      _ = k ^ 3 + 3 * k ^ 2 + 3 * k + 1 + (3 * k + 2) := by algebra
-      _ ≥ k ^ 3 + 3 * k ^ 2 + 3 * k + 1 := by extra
-      _ = (k + 1) ^ 3 := by algebra
+  sorry
 
 
 /-
@@ -247,3 +236,33 @@ example (n : ℕ) : 3 * s2 n ≥ n ^ 3 := by
 -/
 
 -- TODO: bah, no good way of doing this without first establihsing formula for s1
+
+
+
+
+-- Now prove Bernoulli's inequality:
+
+example (x : ℝ) (n : ℕ) (h : x ≥ 0) : (1 + x) ^ n ≥ 1 + n * x := by
+  simple_induction n with k IH
+  · -- base case: goal is `(1 + x) ^ 0 ≥ 1 + 0 * x`
+    sorry
+  · -- inductive step: goal is `(1 + x) ^ (k + 1) ≥ 1 + (k + 1) * x`
+    calc (1 + x) ^ (k + 1) = (1 + x) * (1 + x) ^ k := by algebra
+      _ ≥ (1 + x) * (1 + k * x) := by rel [IH]
+      _ = 1 + (k + 1) * x +  k * x ^ 2 := by algebra
+      _ ≥ 1 + (k + 1) * x := by extra
+
+
+-- Note: in the right window pane you may have seen expressions like `↑k`. These
+-- indicate that the natural number `k` is being considered as a real number.
+
+
+/- # For later use! -/
+
+example (N : ℕ) (h : N ≥ 1) : ∃ n : ℕ, N = n + 1 := by
+  induction_from_starting_point N, h with k hk IH
+  · use 0; numbers
+  · obtain ⟨n, hn⟩ := IH
+    use n + 1
+    rewrite [hn]
+    algebra
